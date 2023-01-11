76b3e6a84adb86286a91b33c61255684cbc2cafb
kagozi <alexkagozi.com>
2023-01-11 19:42:11 +0300

    Completed Semantic HTML

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className='App'>
+      <Header />
+      <Main/>
+      <Footer/>
+    </main>
   );
-}
+};
 
 export default App;
